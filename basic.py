b93cd4f15e34ba7faa177f10bb17bbc3a2e6225c
Ivan Zapata <falenyo@gmail.com>
2021-04-02 12:54:08 -0500

    Created basic.py

---
+++ basic.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(basedir,'data.sqlite')'
+appy.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+Migrate(app,db)
